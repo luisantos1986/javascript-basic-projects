f5bca49e2ef660983cbc36927f18e1e0b2ca0438
Daniel G <yanimames>
2023-01-03 06:27:50 -0600

    identation defitive 2

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -58,21 +58,21 @@ function getRemainingTime() {
     const oneHour = 60 * 60 * 1000;
     const oneMinute = 60 * 1000;
 
-//calculate values 
-   let days = t/oneDay
-   days = Math.floor(days)
-   let hours = Math.floor((t % oneDay) / oneHour);
-   let minutes = Math.floor((t % oneHour) / oneMinute);
-   let seconds = Math.floor((t % oneMinute) / 1000);
+    //calculate values 
+    let days = t/oneDay
+    days = Math.floor(days)
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
 
-// set values array
-  const values = [days,hours,minutes,seconds];
+    // set values array
+    const values = [days,hours,minutes,seconds];
 
     function format(item) {
       if (item < 10) {
           return (item = `0${item}`);
       }
-          return item;
+      return item;
     }
 
     items.forEach(function(items,index) {
